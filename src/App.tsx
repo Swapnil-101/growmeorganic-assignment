3ad2619d3be6715edd01ac0be8f19e2dac9a1259
Swapnil-101 <74921288+Swapnil-101@users.noreply.github.com>
2023-07-24 21:10:54 +0530

    done i think so

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,15 @@
-// import { Routes, Route } from "react-router-dom"
-// import First from "./pages/First.tsx";
-// import Second from "./pages/Second"
-
-
-
-// const App = () => {
-//   const isUserDataAvailable = () => {
-//     const userData = localStorage.getItem('userData');
-//     return !!userData;
-//   };
-//   return (
-//     <div className="App">
-//       <Routes>
-//         <Route path="/" element={<First />} />
-//         <Route path="second" element={<Second />} />
-//       </Routes>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import First from './pages/First.tsx';
 import Second from './pages/Second';
 
 const App = () => {
-  
- 
+
+
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<First />} />
-        
+
         <Route
           path="second"
           element={<Second />}
